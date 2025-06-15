7b3704063bff2115774574feda4cd2d960a994e6
mariaosi <mariaosinnaa22@gmail.com>
2025-06-16 01:29:15 +0300

    Added main class for lines

---
+++ assignment4/Line.h
@@ -0,0 +1,12 @@
+﻿#ifndef LINE_H
+#define LINE_H
+#include <cstdint>
+
+class Line {
+public:
+    virtual uint8_t* serialize(uint8_t* buffer, uint32_t& length) = 0;
+    virtual char* deserialize(uint8_t* buffer, uint32_t length) = 0;
+    virtual Line* copy() = 0;
+};
+
+#endif
