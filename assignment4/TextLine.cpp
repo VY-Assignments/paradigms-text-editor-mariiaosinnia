033d37d1a0434eb6970a42c3eea5472e5a746c11
mariaosi <mariaosinnaa22@gmail.com>
2025-06-16 01:29:45 +0300

    Added TextLine class and its methods realisation

---
+++ assignment4/TextLine.cpp
@@ -0,0 +1,60 @@
+﻿#include "TextLine.h"
+#include <cstring>
+#include <iostream>
+#include <stdint.h>
+
+
+uint8_t* TextLine::serialize(uint8_t* buffer, uint32_t& length) {
+    uint32_t offset = 0;
+    uint32_t text_len = text.length();
+
+    uint8_t prefix_len = strlen(prefix);
+    memcpy(buffer, prefix, prefix_len);
+    offset += prefix_len;
+
+    memcpy(buffer + offset, &text_len, sizeof(text_len));
+    offset += sizeof(text_len);
+
+    memcpy(buffer + offset, text.c_str(), text_len);
+    offset += text_len;
+
+    length = offset;
+
+    return buffer;
+}
+
+char* TextLine::deserialize(uint8_t* buffer, uint32_t length) {
+    uint32_t offset = 0;
+    uint8_t prefix_len = strlen(prefix);
+
+    if (length < prefix_len + sizeof(uint32_t)) {
+        return nullptr;
+    }
+
+    offset += prefix_len;
+
+    uint32_t text_len = 0;
+    memcpy(&text_len, buffer + offset, sizeof(text_len));
+    offset += sizeof(text_len);
+
+    if (offset + text_len > length) {
+        return nullptr;
+    }
+
+    char* result = new char[prefix_len + text_len + 1];
+
+    memcpy(result, prefix, prefix_len);
+    memcpy(result + prefix_len, buffer + offset, text_len);
+    result[prefix_len + text_len] = '\0';
+
+    return result;
+}
+
+Line *TextLine::copy() {
+    return new TextLine(this->text);
+}
+
+
+
+
+
---
+++ assignment4/TextLine.h
@@ -0,0 +1,20 @@
+﻿#ifndef TEXTLINE_H
+#define TEXTLINE_H
+#include <stdint.h>
+#include <string>
+#include "Line.h"
+
+class TextLine : public Line{
+private:
+    std::string text;
+    const char* prefix = "Text: ";
+public:
+    TextLine(std::string& t) : text(t) {}
+    uint8_t* serialize(uint8_t* buffer, uint32_t& length) override;
+    char* deserialize(uint8_t* buffer, uint32_t length) override;
+    Line* copy() override;
+};
+
+
+
+#endif
