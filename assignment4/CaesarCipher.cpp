b83d1fd146dc1068cf40d0d00c484321c4ef9664
mariaosi <mariaosinnaa22@gmail.com>
2025-06-20 19:24:51 +0300

    Added caesar cipher class

---
+++ assignment4/CaesarCipher.cpp
@@ -0,0 +1,39 @@
+﻿#include "CaesarCipher.h"
+
+#include <combaseapi.h>
+#include <cstdio>
+
+CaesarCipher::CaesarCipher(char *dll_name) {
+    HINSTANCE handle = LoadLibrary(TEXT("caesar.dll"));
+    if (handle == nullptr || handle == INVALID_HANDLE_VALUE) {
+        printf("Lib is not found");
+        return;
+    }
+    encrypt_ptr = (encrypt_ptr_t)GetProcAddress(handle, "encrypt");
+    if (encrypt_ptr == nullptr) {
+        printf("Lib is not found");
+        return;
+    }
+
+    decrypt_ptr = (decrypt_ptr_t)GetProcAddress(handle, "decrypt");
+    if (decrypt_ptr == nullptr) {
+        printf("Lib is not found");
+        return;
+    }
+}
+
+char* CaesarCipher::encrypt(char* rawText, int key) {
+    return encrypt_ptr(rawText, key);
+}
+
+char* CaesarCipher::decrypt(char* encryptedText, int key) {
+    return decrypt_ptr(encryptedText, key);
+}
+
+
+CaesarCipher::~CaesarCipher() {
+    FreeLibrary(handle);
+}
+
+
+
---
+++ assignment4/CaesarCipher.h
@@ -0,0 +1,26 @@
+﻿
+
+#ifndef CAESARCIPHER_H
+#define CAESARCIPHER_H
+#include <bemapiset.h>
+
+
+class CaesarCipher {
+public:
+    CaesarCipher(char* dll_name);
+    ~CaesarCipher();
+
+    char* encrypt(char* rawText, int key);
+    char* decrypt(char* encryptedText, int key);
+private:
+    HINSTANCE handle;
+    typedef char*(*encrypt_ptr_t)(char*, int);
+    typedef char*(*decrypt_ptr_t)(char*, int);
+
+    encrypt_ptr_t encrypt_ptr = nullptr;
+    decrypt_ptr_t decrypt_ptr = nullptr;
+    };
+
+
+
+#endif
