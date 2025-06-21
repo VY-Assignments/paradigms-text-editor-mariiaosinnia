a9a15c8e89b57c2a8a1b7e71a8f68e44ed11c28b
mariaosi <mariaosinnaa22@gmail.com>
2025-06-21 20:49:54 +0300

    Changed CheckListLine class

--- assignment4/CheckListLine.cpp
+++ assignment4/CheckListLine.cpp
@@ -4,7 +4,7 @@
 
 uint8_t* CheckListLine::serialize(uint32_t &length) {
     uint32_t offset = 0;
-    uint32_t task_len = task.length();
+    uint32_t task_len = strlen(task);
     uint8_t prefix_len = strlen(prefix_task_completed);
 
     length = prefix_len + sizeof(task_len) + task_len;
@@ -21,7 +21,7 @@ uint8_t* CheckListLine::serialize(uint32_t &length) {
     memcpy(buffer + offset, &task_len, sizeof(task_len));
     offset += sizeof(task_len);
 
-    memcpy(buffer + offset, task.c_str(), task_len);
+    memcpy(buffer + offset, task, task_len);
     offset += task_len;
 
     return buffer;
@@ -58,12 +58,41 @@ void CheckListLine::deserialize(uint8_t *buffer, uint32_t length) {
     memcpy(result + prefix_len, buffer + offset, task_len);
     result[prefix_len + task_len] = '\0';
 
-    task = std::string(result);
-    delete result;
+    task = result;
 }
 
 Line* CheckListLine::copy() {
     return new CheckListLine(this->task, this->is_completed);
 }
+
+char *CheckListLine::get_type() {
+    return "Check list";
+}
+
+
+void CheckListLine::append_text(char *input) {
+    const char* prefix;
+    if (is_completed) {
+        prefix = prefix_task_completed;
+    } else {
+        prefix = prefix_task_uncompleted;
+    }
+    int prefix_len = strlen(prefix);
+    int input_len = strlen(input);
+    int old_len = 0;
+    if (task != nullptr) {
+        old_len = strlen(task);
+    }
+
+    char* new_task = (char*)realloc(task, old_len + prefix_len + input_len + 1);
+    if (!new_task) return;
+
+    memcpy(new_task + old_len, prefix, prefix_len);
+    memcpy(new_task + old_len + prefix_len, input, input_len + 1);
+
+    task = new_task;
+    length = old_len + prefix_len + input_len;
+}
+
 
 
--- assignment4/CheckListLine.h
+++ assignment4/CheckListLine.h
@@ -8,16 +8,18 @@
 class CheckListLine : public Line{
 private:
     bool is_completed;
-    std::string task;
-    const char* prefix_task_completed = "[ ]";
-    const char* prefix_task_uncompleted = "[x]";
+    char* task;
+    int length;
+    const char* prefix_task_uncompleted = "[ ] ";
+    const char* prefix_task_completed = "[x] ";
 public:
-    CheckListLine(std::string& task) : task(task) {};
-    CheckListLine(const std::string& task, bool is_completed)
+    CheckListLine(char* task, bool is_completed)
     : task(task), is_completed(is_completed) {}
     uint8_t* serialize(uint32_t &length) override;
     void deserialize(uint8_t *buffer, uint32_t length) override;
     Line* copy() override;
+    char* get_type();
+    void append_text(char* input);
 };
 
 
